f50c1b10460992d52cf6faa49d29acd090855c7c
Paul Forshaw <Paul.Forshaw@uk.ibm.com>
2020-09-25 09:59:28 +0100

    Revert "Modified index.php"
    This reverts commit 526abb7713ee0a41f28c8ad39a1e3781aab7299a.

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,5 +1,4 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "A change is a coming!\n";
-print "Changed by Paul Forshaw in pf-s2i branch! \n";
+print "A change is a coming!\n"
 ?>
