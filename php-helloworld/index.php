4224617bff59b7092a8fc24e602d7d9f189591f8
Paul Forshaw <Paul.Forshaw@uk.ibm.com>
2020-09-25 10:15:28 +0100

    Mod 2 to index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,5 +1,5 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
 print "A change is a coming!\n";
-print "Modified by PF in branch pf-s2i\n";
+print "Modified by PF in branch pf-s2i and changed again for build 2!\n";
 ?>
