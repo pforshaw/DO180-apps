526abb7713ee0a41f28c8ad39a1e3781aab7299a
Paul Forshaw <Paul.Forshaw@uk.ibm.com>
2020-09-25 09:51:00 +0100

    Modified index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,5 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "A change is a coming!\n"
+print "A change is a coming!\n";
+print "Changed by Paul Forshaw in pf-s2i branch! \n";
 ?>
